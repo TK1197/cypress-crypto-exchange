a95da0b014eac543d75f8157a057ae7ed9ad3b55
ThibanKumar <thibankj@gmail.com>
2023-04-21 04:44:29 +0800

    - added url into global config - declare first function - added first test case

---
+++ cypress/integration/keywords/crypto_exchange.ts
@@ -0,0 +1,16 @@
+///<reference types="Cypress" />
+
+class crypto_exchange{
+
+    callWebURL(){
+
+        cy.visit(Cypress.env('url'))
+    }
+
+    validateHomePageComponent(){
+
+       
+    }
+}
+
+export default crypto_exchange;
